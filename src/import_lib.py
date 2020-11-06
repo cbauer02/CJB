6c2e50ca67133222effe70bf9ad8b4ea0cbcd00b
cbauer02 <cbauer02@gmail.com>
2020-11-06 15:06:45 -0800

    Update code in branch split_func

---
+++ src/import_lib.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import numpy as np
+import lxml.html as lh
+
+print("Libraries imported")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-import liquor_search_scraping
+import import_lib
+import liquor_search_list_scraping
+import liquor_search_inv_scraping
 
 print("Running scrape_olcc_whiskey_list & scrape_olcc_whiskey_inv")
-print(liquor_search_scraping.scrape_olcc_whiskey_list("http://www.oregonliquorsearch.com/"))
-print(liquor_search_scraping.scrape_olcc_whiskey_inv("http://www.oregonliquorsearch.com/"))
+import_lib
+print(liquor_search_list_scraping.scrape_olcc_whiskey_list("http://www.oregonliquorsearch.com/"))
+print(liquor_search_inv_scraping.scrape_olcc_whiskey_inv("http://www.oregonliquorsearch.com/"))
